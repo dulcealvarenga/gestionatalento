27fc87560ec80394f229c646e494915c37bfb384
dulce <dulcealvarengamen@fpuna.edu.py>
2025-05-14 22:10:19 -0300

    Se agrega opcion de editar/eliminar desc salariales

--- gestionatalento/src/components/DescSalariales.jsx
+++ gestionatalento/src/components/DescSalariales.jsx
@@ -2,17 +2,22 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./DescSalariales.css";
 import axios from "axios";
+import { toast } from 'react-toastify';
+
 
 const DescuentosSalariales = () => {
-
     const [descuentos, setDescuentos] = useState([]);
     const navigate = useNavigate();
+    const [showModal, setShowModal] = useState(false);
+    const [selectedDescuento, setSelectedDescuento] = useState(null);
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
+    const [descuentoToDelete, setDescuentoToDelete] = useState(null);
 
     const fetchDescuentos = async () => {
         try {
             const response = await axios.get('http://localhost:8080/descuentos-salariales/obtenerLista');
-            console.log(response.data.objeto);
             setDescuentos(response.data.objeto || []);
+            console.log(response.data);
         } catch (error) {
             console.error("Error al obtener descuentos:", error);
             setDescuentos([]);
@@ -22,6 +27,57 @@ const DescuentosSalariales = () => {
     useEffect(() => {
         fetchDescuentos();
     }, []);
+
+    const handleEliminar = async () => {
+        try {
+            await axios.delete('http://localhost:8080/descuentos-salariales/eliminar', {
+                data: {
+                    empleado: { codEmpleado: descuentoToDelete.empleado.codEmpleado },
+                    codPeriodo: descuentoToDelete.id.codPeriodo
+                }
+            });
+            toast.success("Registro eliminado correctamente", { autoClose: 2000 });
+            setShowConfirmModal(false);
+            fetchDescuentos();
+        } catch (error) {
+            console.error("Error al eliminar:", error);
+            toast.error("No se pudo eliminar el registro", { autoClose: 2000 });
+        }
+    };
+
+
+    const openModal = (descuento) => {
+        setSelectedDescuento({ ...descuento });  // Clon para edición
+        setShowModal(true);
+    };
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setSelectedDescuento(prev => ({ ...prev, [name]: value }));
+    };
+
+    const handleSave = async () => {
+        try {
+            await axios.put(`http://localhost:8080/descuentos-salariales/actualizar`, {
+                empleado: {
+                    codEmpleado: selectedDescuento.empleado.codEmpleado
+                },
+                codPeriodo: selectedDescuento.id.codPeriodo,
+                entradaTardia: parseInt(selectedDescuento.entradaTardia),
+                salidaAnticipada: parseInt(selectedDescuento.salidaAnticipada),
+                ausencia: parseInt(selectedDescuento.ausencia),
+                monto: parseFloat(selectedDescuento.monto),
+                observacion: "Modificado desde sistema web"
+            });
+
+            setShowModal(false);
+            fetchDescuentos();
+        } catch (error) {
+            console.error("Error al actualizar:", error);
+            alert("No se pudo actualizar el registro.");
+        }
+    };
+
 
     return (
         <div className="descuentos-container">
@@ -36,8 +92,9 @@ const DescuentosSalariales = () => {
                 </select>
                 <button className="btn-buscar">BUSCAR</button>
             </div>
+
             <div className="acciones-buttons">
-            <button>EXPORTAR MARCACIONES</button>
+                <button>EXPORTAR MARCACIONES</button>
                 <button>EXONERADOS</button>
                 <button>EXPORTAR BORRADOR</button>
                 <button>EXPORTAR RECIBIDOS</button>
@@ -55,13 +112,13 @@ const DescuentosSalariales = () => {
                     <th>Ausencias</th>
                     <th>Monto Descuento</th>
                     <th>Dependencia</th>
-                    <th></th>
+                    <th>Acciones</th>
                 </tr>
                 </thead>
                 <tbody>
                 {descuentos.map((d, index) => (
                     <tr key={index}>
-                        <td>{d.codPeriodo}</td>
+                        <td>{d.id.codPeriodo}</td>
                         <td>{d.empleado.persona.nroDocumento}</td>
                         <td>{(d.empleado.persona.nombres || "") + " " + (d.empleado.persona.apellidos || "")}</td>
                         <td>{d.empleado.asignacion}</td>
@@ -70,11 +127,75 @@ const DescuentosSalariales = () => {
                         <td>{d.ausencia}</td>
                         <td>{d.monto}</td>
                         <td>{d.empleado.cargo.departamento.direccion.descripcion}</td>
-                        <td></td>
+                        <td>
+                            <button className="btn-accion"
+                                    onClick={() => openModal(d)}>
+                                <img src="/public/editar.png" alt="Editar" className="icono-accion"/>
+                            </button>
+                            <button className="btn-accion" onClick={() => {
+                                setDescuentoToDelete(d);
+                                setShowConfirmModal(true);
+                            }}>
+                                <img src="/public/borrar.png" alt="Eliminar" className="icono-accion"/>
+                            </button>
+                        </td>
                     </tr>
                 ))}
                 </tbody>
             </table>
+            {showConfirmModal && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <h2>Confirmar eliminación</h2>
+                        <p>¿Estás seguro que querés eliminar este registro?</p>
+                        <div className="modal-buttons">
+                            <button onClick={() => setShowConfirmModal(false)}>Cancelar</button>
+                            <button onClick={handleEliminar}>Confirmar</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+            {showModal && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <h2>Editar Descuento</h2>
+
+                        <div className="modal-info">
+                            <div>
+                                <label>Documento:</label>
+                                <span>{selectedDescuento?.empleado?.persona?.nroDocumento}</span>
+                            </div>
+                            <div>
+                                <label>Nombre:</label>
+                                <span>{`${selectedDescuento?.empleado?.persona?.nombres || ""} ${selectedDescuento?.empleado?.persona?.apellidos || ""}`}</span>
+                            </div>
+                        </div>
+
+                        <div className="modal-grid">
+                            <label>Entradas Tardías
+                                <input type="number" name="entradaTardia" value={selectedDescuento.entradaTardia} onChange={handleInputChange} />
+                            </label>
+
+                            <label>Salidas Anticipadas
+                                <input type="number" name="salidaAnticipada" value={selectedDescuento.salidaAnticipada} onChange={handleInputChange} />
+                            </label>
+
+                            <label>Ausencias
+                                <input type="number" name="ausencia" value={selectedDescuento.ausencia} onChange={handleInputChange} />
+                            </label>
+
+                            <label>Monto Descuento
+                                <input type="number" name="monto" value={selectedDescuento.monto} onChange={handleInputChange} />
+                            </label>
+                        </div>
+
+                        <div className="modal-buttons">
+                            <button onClick={() => setShowModal(false)}>Cancelar</button>
+                            <button onClick={handleSave}>Guardar</button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
